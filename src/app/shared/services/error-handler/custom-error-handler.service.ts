96ab7d53ed1f9124edc39a40c94e3bfdd2d3889f
Amit Patel <akpatel7@users.noreply.github.com>
2022-05-23 20:19:04 -0400

    Formatted stack trace to be parsable via source map on server

--- src/app/shared/services/error-handler/custom-error-handler.service.ts
+++ src/app/shared/services/error-handler/custom-error-handler.service.ts
@@ -3,17 +3,6 @@ import * as StackTrace from 'stacktrace-js';
 import { NGXLogger } from 'ngx-logger';
 // import { LogService } from '../../../shared/services/log/log.service';
 
-
-// const errback = (err: { message: any; }) => {
-//   console.error('window.onerror: ' + err.message);
-// }
-
-// const callback = (stackframes: any[]) => {
-//       let stringifiedStack = stackframes.map(function(sf: { toString: () => any; }) {
-//         return sf.toString();
-//       }).join('\n');
-//     console.error('window.onerror: ' + stringifiedStack);
-//}
 @Injectable()
 export class CustomErrorHandlerService extends ErrorHandler {
 
@@ -21,18 +10,8 @@ export class CustomErrorHandlerService extends ErrorHandler {
         super();
     }
 
-  // constructor(
-  //   private logger: NGXLogger, private window: Window) {
-  //   // window.onerror = function(msg, file, line, col, error ) {
-  //   //   if (error) {
-  //   //     StackTrace.fromError(error).then(callback).catch(errback);
-  //   //   }
-  //   // }
-  //   super();
-  // }
-
     public async handleError(error: any) {
-      let errorMessage = '', stackTrace, reportObject;
+      let errorMessage = '', stackFrameDetail, reportObject;
 
       errorMessage = error?.error?.message ? error?.message : error?.toString() || '';
 
@@ -50,30 +29,38 @@ export class CustomErrorHandlerService extends ErrorHandler {
           };
           this.logger.error(reportObject);
         }
-        else {
+        else
+        {
           // client-side error
           await StackTrace.fromError(error).then((stackframes) => {
-            stackTrace = stackframes
-              .splice(0, 10)
-              .map(function(sf) {
-                return sf.toString();
-              });
-              //.toString();
-
-          // StackTrace.get().then((stack) => {
-          //   const stackString = stack;
-              // .splice(0, 10)
-              // .map(function(sf) {
-              //   return sf.toString();
-              // })
-              // .toString();
-
-            // const errorTraceStr = `Error message: ${message}. Stack trace: ${stackString}`;
+            stackFrameDetail = [];
+            for(let sf of stackframes) {
+              const {
+                functionName,
+                args,
+                fileName,
+                lineNumber,
+                columnNumber,
+                source,
+                isEval,
+                isNative
+              } = sf;
+              stackFrameDetail.push({
+                  functionName,
+                  args,
+                  fileName,
+                  lineNumber,
+                  columnNumber,
+                  source,
+                  isEval,
+                  isNative
+                });
+            }
           });
           reportObject = {
             name: error.name,
             message: errorMessage || error.message,
-            stack: stackTrace || error.stack,
+            stack: stackFrameDetail, // || error.stack,
             url: 'location.href',
             route: 'router.url',
           };
@@ -82,10 +69,32 @@ export class CustomErrorHandlerService extends ErrorHandler {
       }
       catch(err) {
         console.error('FATAL ERROR in error handler: ', err);
-        throw err;
+        this.handleError(err);
       }
-      finally {
+      finally{
         super.handleError(error);
       }
     }
 }
+
+/* window onerror version */
+/* https://stackoverflow.com/questions/34001666/how-do-i-get-a-simple-stacktrace-js-example-working
+ window.onerror = function(msg, file, line, col, error) {
+  StackTrace.fromError(error).then(callback).catch(errback);
+}
+var errback = function(err) { console.log(err.message);
+};
+var callback = function(stackframes) {
+    var stringifiedStack = stackframes.map(function(sf) {
+      return sf.toString();
+    }).join('\n');  };
+function fault() {
+
+alert('Error will be invoked!')
+  throw "Test Error"
+};
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/stacktrace.js/2.0.0/stacktrace.js"></script>
+
+ */
